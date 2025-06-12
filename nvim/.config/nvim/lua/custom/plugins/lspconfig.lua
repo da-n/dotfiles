77c1a8ac951ef2fd19789f811f5d76db6e669404
Daniel Davidson <dan@bitmono.com>
2025-06-12 09:58:15 +0100

    Update lspconfig for new format, add neoscroll

--- nvim/.config/nvim/lua/custom/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/custom/plugins/lspconfig.lua
@@ -203,75 +203,31 @@ return {
         enabled = false,
       }
 
-      -- Enable the following language servers
-      --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-      --
-      --  Add any additional override configuration in the following tables. Available keys are:
-      --  - cmd (table): Override the default command used to start the server
-      --  - filetypes (table): Override the default list of associated filetypes for the server
-      --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-      --  - settings (table): Override the default settings passed when initializing the server.
-      --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-      local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
-
-        lua_ls = {
-          -- cmd = { ... },
-          -- filetypes = { ... },
-          -- capabilities = {},
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
+      vim.lsp.config('lua_ls', {
+        -- cmd = { ... },
+        -- filetypes = { ... },
+        -- capabilities = {},
+        settings = {
+          Lua = {
+            completion = {
+              callSnippet = 'Replace',
             },
+            -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+            -- diagnostics = { disable = { 'missing-fields' } },
           },
         },
-        gopls = {
-          settings = {
-            gopls = {
-              usePlaceholders = true,
-              buildFlags = { '-tags=debug,test,service_tests,system_tests' },
-              expandWorkspaceToModule = true,
-              ['local'] = 'gitlab.com/adalpha/pillar',
-              gofumpt = true,
-            },
-          },
-        },
-        marksman = {},
-      }
-
-      -- Ensure the servers and tools above are installed
-      --
-      -- To check the current status of installed tools and/or manually install
-      -- other tools, you can run
-      --    :Mason
-      --
-      -- You can press `g?` for help in this menu.
-      --
-      -- `mason` had to be setup earlier: to configure its options see the
-      -- `dependencies` table for `nvim-lspconfig` above.
-      --
-      -- You can add other tools here that you want Mason to install
-      -- for you, so that they are available from within Neovim.
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
       })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+      vim.lsp.config('gopls', {
+        settings = {
+          gopls = {
+            usePlaceholders = true,
+            buildFlags = { '-tags=debug,test,service_tests,system_tests' },
+            expandWorkspaceToModule = true,
+            ['local'] = 'gitlab.com/adalpha/pillar',
+            gofumpt = true,
+          },
+        },
+      })
 
       require('lspconfig').csharp_ls.setup {
         cmd = { '/home/daniel/.dotnet/tools/csharp-ls' },
@@ -281,6 +237,7 @@ return {
       require('mason-lspconfig').setup {
         ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
         automatic_installation = false,
+        automatic_enable = true,
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
